ff33fc68fe33b7ff2e3a0696dfa25e4159e3ea55
Charles Flèche <cfleche@rodeofx.com>
2019-04-24 17:16:24 -0400

    Extract stub FindUSD module

---
+++ cmake/modules/FindUSD.cmake
@@ -0,0 +1,10 @@
+if(EXISTS ${USD_LOCATION}/pxrConfig.cmake)
+  include(${USD_LOCATION}/pxrConfig.cmake)
+endif()
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(USD
+  DEFAULT_MSG
+  PXR_INCLUDE_DIRS
+  PXR_LIBRARIES
+)
