d8822b24289629d0c0e7eea9c6ee4ea0ff6fe9a7
Charles Flèche <cfleche@rodeofx.com>
2019-04-25 17:52:16 -0400

    Import Sdf for Houdini

--- src/testenv/__init__.py
+++ src/testenv/__init__.py
@@ -4,7 +4,9 @@
 #
 # There is no way from the pxrConfig.cmake file to get library paths of USD
 # dependencies like boost python, tbb, etc
-# Importing Tf here ensures that all required symbols are available before
+# Importing Sdf here ensures that all required symbols are available before
 # importing rdo.ReplaceResolver
+#
+# For some reason import Tf is not enough in Houdini: Sdf needs to be imported
 
-import pxr.Tf
+import pxr.Sdf
