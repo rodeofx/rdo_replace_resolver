f89e20cd1d93db78265e2c7d638c4a4f2e511413
Charles Flèche <cfleche@rodeofx.com>
2019-04-29 09:48:11 -0400

    Remove pragma #once

--- src/boost_include_wrapper.h
+++ src/boost_include_wrapper.h
@@ -1,7 +1,10 @@
-#pragma once
+#ifnded BOOST_INCLUDE_WRAPPER_H
+#define BOOST_INCLUDE_WRAPPER_H
 
 #ifndef BOOST_NAMESPACE
   #define BOOST_NAMESPACE boost
 #endif
 
 #define BOOST_INCLUDE(path) <BOOST_NAMESPACE/path>
+
+#endif // BOOST_INCLUDE_WRAPPER_H
